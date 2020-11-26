f40306cb4696829db15440cc01c5c062c92415d6
iblue <iblue@DESKTOP-F3S15KD.mshome.net>
2020-11-26 10:29:54 +0900

    create api gateway

---
+++ src/main/java/com/openapilegacy/OpenapilegacyBApplication.java
@@ -0,0 +1,13 @@
+package com.openapilegacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapilegacyBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapilegacyBApplication.class, args);
+	}
+
+}
